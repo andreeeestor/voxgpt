ce87690f840731fa163874ec11a907f20ab421b9
Andre Nestor <andrenestor0503@gmail.com>
2024-03-02 19:51:37 -0300

    new home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import NextTopLoader from "nextjs-toploader";
@@ -8,6 +8,8 @@ import SidebarProvider from "@/data/contexts/SidebarContext";
 import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const spaceGrotesk = Space_Grotesk({  subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] });
 
 export const metadata: Metadata = {
   title: "VoxGPT",
@@ -22,7 +24,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="pt-br" data-theme="dark">
-        <body className={`${inter.className} antialiased`}>
+        <body className={`${spaceGrotesk.className} antialiased`}>
           <NextTopLoader height={5} color="#6691ff" />
           <Toaster richColors />
           <Providers>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,31 @@
+import { WavyBackground } from "@/components/ui/wavy-background";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen bodyBackground">
-      <article className="max-w-md text-center space-y-6">
-        <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-700">VoxGPT</h1>
-        <p className="text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam quo,
-          numquam doloribus quae incidunt libero nulla officiis sequi expedita
-          animi placeat ut perferendis nobis. Voluptates eveniet porro assumenda
-          earuma.
-        </p>
-        <Link href={`/chat`} className="btn btn-primary">
-          COMEÇAR
-        </Link>
-      </article>
-    </section>
+    <WavyBackground>
+      <section className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center text-white">
+        <div className="mx-auto max-w-3xl text-center">
+          <h1 className="bg-gradient-to-r from-green-400 via-blue-600 to-purple-700 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+            VoxGPT
+          </h1>
+
+          <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+            VoxGPT: Seu companheiro, guia turístico virtual e
+            inesquecível. <br />
+            Planeje sua próxima aventura com a ajuda da inteligência artificial!
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <Link
+              href={`/chat`}
+              className="shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-blue-600 rounded-md text-white font-light transition duration-200 ease-linear"
+            >
+              Começar
+            </Link>
+          </div>
+        </div>
+      </section>
+    </WavyBackground>
   );
 }
